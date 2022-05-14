6225f3ecb109b0320ecdba397464cfc861be2862
czq <435201823@qq.com>
2022-05-14 16:20:20 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,40 @@
 <template>
-  <section class="el-container">
-    <aside class="el-aside" style="width: 200px">Aside</aside>
-    <section class="el-container is-vertical">
-      <header class="el-header" style="height: 60px">Header</header>
-      <main class="el-main">Main</main>
-    </section>
-  </section>
+  <el-menu
+    default-active="0"
+    class="el-menu-vertical-demo"
+    @open="handleOpen"
+    @close="handleClose"
+    :collapse="isCollapse"
+  >
+    <el-menu-item index="1" backgroundColor="#545c64">
+      <i class="el-icon-setting"></i>
+      <span slot="title">导航四</span>
+    </el-menu-item>
+  </el-menu>
 </template>
-
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
+  data() {
+    return {
+      isCollapse: true,
+    };
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+  background-color: aquamarine;
 }
 </style>
