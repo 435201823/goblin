a672f4ca26b79427789fad8aff672af78f031868
czq <435201823@qq.com>
2022-05-16 00:01:26 +0800

    增加‘新增会话按钮以及模态框’

--- src/components/HostManage.vue
+++ src/components/HostManage.vue
@@ -1,6 +1,57 @@
-<template><div>adsfasdfasdfasdfasdf</div></template>
+<template>
+  <div>
+    <el-col :span="1">
+      <el-button @click="dialogFormVisible = true">新增会话</el-button>
+    </el-col>
+    <el-dialog title="连接地址" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="名称" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="地址" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="用户名" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.name"
+            autocomplete="off"
+            show-password
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false"
+          >确 定</el-button
+        >
+      </div>
+    </el-dialog>
+  </div>
+</template>
 
 <style></style>
 
 <script>
+export default {
+  data() {
+    return {
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
+      },
+      formLabelWidth: "120px",
+    };
+  },
+};
 </script>
