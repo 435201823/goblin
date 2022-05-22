95058eb2421d8f7bc4650deebaf9058aebbfed04
czq <435201823@qq.com>
2022-05-22 14:45:48 +0800

    feat: 增加tree组件

--- src/components/HostManage.vue
+++ src/components/HostManage.vue
@@ -1,36 +1,41 @@
 <template>
   <div>
-    <el-col :span="1">
-      <el-button @click="dialogFormVisible = true">新增会话</el-button>
-    </el-col>
-    <el-dialog
-      title="连接地址"
-      :visible.sync="dialogFormVisible"
-      @close="closeDialog"
-    >
-      <el-form :model="form">
-        <el-form-item label="名称" :label-width="formLabelWidth">
-          <el-input v-model="form.session_name" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="地址" :label-width="formLabelWidth">
-          <el-input v-model="form.addr" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="用户名" :label-width="formLabelWidth">
-          <el-input v-model="form.username" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" :label-width="formLabelWidth">
-          <el-input
-            v-model="form.password"
-            autocomplete="off"
-            show-password
-          ></el-input>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="customCommand()">确 定</el-button>
-      </div>
-    </el-dialog>
+    <el-row>
+      <el-col :span="1">
+        <el-button @click="dialogFormVisible = true">新增会话</el-button>
+      </el-col>
+      <el-dialog
+        title="连接地址"
+        :visible.sync="dialogFormVisible"
+        @close="closeDialog"
+      >
+        <el-form :model="form">
+          <el-form-item label="名称" :label-width="formLabelWidth">
+            <el-input v-model="form.session_name" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="地址" :label-width="formLabelWidth">
+            <el-input v-model="form.addr" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="用户名" :label-width="formLabelWidth">
+            <el-input v-model="form.username" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" :label-width="formLabelWidth">
+            <el-input
+              v-model="form.password"
+              autocomplete="off"
+              show-password
+            ></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="customCommand()">确 定</el-button>
+        </div>
+      </el-dialog>
+    </el-row>
+    <el-row>
+      <SessionList />
+    </el-row>
   </div>
 </template>
 
@@ -38,6 +43,7 @@
 
 <script>
 import { invoke } from "@tauri-apps/api/tauri";
+import SessionList from "./SessionList.vue";
 
 export default {
   data() {
@@ -65,5 +71,6 @@ export default {
       this.form.password = "";
     },
   },
+  components: { SessionList },
 };
 </script>
