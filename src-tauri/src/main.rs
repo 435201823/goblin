52a9764e46fc441998d2b8073f87085534a78d44
czq <435201823@qq.com>
2022-05-22 14:00:19 +0800

    feat:保存session

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,17 +3,23 @@
     windows_subsystem = "windows"
 )]
 
+use goblin_core::api::GoblinCore;
+use goblin_core::session::Session;
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![save_session])
+        .invoke_handler(tauri::generate_handler![create_session])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
 #[tauri::command]
-fn save_session(name:String,addr:String) {
-  println!("I was invoked from JS:{}",name);
-  println!("I was invoked from JS:{}",addr);
+pub(crate) fn create_session(sessionname: String, addr: String, username: String, password: String) {
+    let session = Session{
+        name: sessionname,
+        addr,
+        username,
+        password
+    };
+    GoblinCore::create_session(session);
 }
-
-//conn_name:String,addr:String,username:String,psw:String
--- src/components/HostManage.vue
+++ src/components/HostManage.vue
@@ -3,10 +3,14 @@
     <el-col :span="1">
       <el-button @click="dialogFormVisible = true">新增会话</el-button>
     </el-col>
-    <el-dialog title="连接地址" :visible.sync="dialogFormVisible">
+    <el-dialog
+      title="连接地址"
+      :visible.sync="dialogFormVisible"
+      @close="closeDialog"
+    >
       <el-form :model="form">
         <el-form-item label="名称" :label-width="formLabelWidth">
-          <el-input v-model="form.name" autocomplete="off"></el-input>
+          <el-input v-model="form.session_name" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="地址" :label-width="formLabelWidth">
           <el-input v-model="form.addr" autocomplete="off"></el-input>
@@ -16,7 +20,7 @@
         </el-form-item>
         <el-form-item label="密码" :label-width="formLabelWidth">
           <el-input
-            v-model="form.psw"
+            v-model="form.password"
             autocomplete="off"
             show-password
           ></el-input>
@@ -47,17 +51,18 @@ export default {
   methods: {
     customCommand() {
       // Invoke the command
-      invoke("save_session", {
-        name: this.form.name,
+      invoke("create_session", {
+        sessionname: this.form.session_name,
         addr: this.form.addr,
+        username: this.form.username,
+        password: this.form.password,
       });
-      // {
-      //   name: this.form.name,
-      //   addr: this.form.addr,
-      //   username: this.form.username,
-      //   psw: this.form.psw,
-      // }
-      // dialogFormVisible = false;
+    },
+    closeDialog() {
+      this.form.session_name = "";
+      this.form.addr = "";
+      this.form.username = "";
+      this.form.password = "";
     },
   },
 };
